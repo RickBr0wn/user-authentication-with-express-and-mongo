dfc656b69ba7a3d08e4320ed66b34c8f5ffad787
RiCK BROWN <ricky.brown.00@gmail.com>
2018-03-03 20:30:24 +0000

    Added middleware for loggedOut

--- app.js
+++ app.js
@@ -2,14 +2,25 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
 var app = express();
 
 // middle ware
+// mongoDB connection
+mongoose.connect("mongodb://localhost:27017/bookworm");
+var db = mongoose.connection;
+
+// mongoDB error handling
+db.on('error', console.error.bind(console, 'connection error:'));
+
 // use sessions for tracking logins
 app.use(session({
   secret: 'Rick Brown 2018',
   resave: true,
-  saveUninitialized: false
+  saveUninitialized: false,
+  store: new MongoStore({
+    mongooseConnection: db
+  })
 }));
 
 // make userID avaiable to all templates
@@ -17,13 +28,6 @@ app.use(function(req, res, next){
   res.locals.currentUser = req.session.userId;
   next();
 });
-
-// mongoDB connection
-mongoose.connect("mongodb://localhost:27017/bookworm");
-var db = mongoose.connection;
-
-// mongoDB error handling
-db.on('error', console.error.bind(console, 'connection error:'));
 
 // parse incoming requests
 app.use(bodyParser.json());
