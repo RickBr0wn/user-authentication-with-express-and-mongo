3d360188df5a1a0670432c2bff8d3c6184509bcc
RiCK BROWN <ricky.brown.00@gmail.com>
2018-03-03 16:34:33 +0000

    Initial Commit

---
+++ routes/index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+// GET /
+router.get('/', function(req, res, next) {
+  return res.render('index', { title: 'Home' });
+});
+
+// GET /register
+router.get('/register', function(req, res, next){
+  return res.render('register', { title: 'Register' });
+});
+
+// POST /register
+router.post('/register', function(res, req, next){
+  return res.send('User created');
+});
+
+// GET /about
+router.get('/about', function(req, res, next) {
+  return res.render('about', { title: 'About' });
+});
+
+// GET /contact
+router.get('/contact', function(req, res, next) {
+  return res.render('contact', { title: 'Contact' });
+});
+
+module.exports = router;
