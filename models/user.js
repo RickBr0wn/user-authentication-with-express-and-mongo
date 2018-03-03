74af88185c4cc5066758191d4cf369c1528fd066
RiCK BROWN <ricky.brown.00@gmail.com>
2018-03-03 17:49:47 +0000

    Added login logic

--- models/user.js
+++ models/user.js
@@ -2,38 +2,61 @@ var mongoose = require('mongoose');
 var bcrypt = require('bcrypt');
 
 var UserSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    unique: true,
-  },
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  favoriteBook: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  password: {
-    type: String,
-    required: true
-  }
+	email: {
+		type: String,
+		required: true,
+		trim: true,
+		unique: true,
+	},
+	name: {
+		type: String,
+		required: true,
+		trim: true,
+	},
+	favoriteBook: {
+		type: String,
+		required: true,
+		trim: true
+	},
+	password: {
+		type: String,
+		required: true
+	}
 });
 
+// authenticate input against database documents
+UserSchema.statics.authenticate = function (email, password, callback) {
+	User.findOne({
+			email: email
+		})
+		.exec(function (error, user) {
+			if (error) {
+				return callback(error);
+			} else if (!user) {
+				var err = new Error('User not found.');
+				err.status = 401;
+				return callback(err);
+			}
+			bcrypt.compare(password, user.password, function(error, result){
+				if(result){
+					return callback(null, user);
+				}else{
+					return callback();
+				}
+			});
+		});
+}
+
 // hash password before saving to mongoDB
-UserSchema.pre('save', function(next){
-    var user = this;
-    bcrypt.hash(user.password, 10, function(err, hash){
-       if(err){
-           return next(err);
-       }
-       user.password = hash;
-       next(); 
-    });
+UserSchema.pre('save', function (next) {
+	var user = this;
+	bcrypt.hash(user.password, 10, function (err, hash) {
+		if (err) {
+			return next(err);
+		}
+		user.password = hash;
+		next();
+	});
 });
 
 var User = mongoose.model('User', UserSchema);
